b5bfb86f6bfe0de1905fc6cd5eb2a476cbd7a1ae
josefbutts <buttsjosef@gmail.com>
2018-08-14 14:34:12 -0500

    Edited information in server.js as to how I redirected the AWS server

--- server.js
+++ server.js
@@ -14,3 +14,9 @@ helpers.searchByAddress('1403+Washington+Ave');
 app.listen(8200, function() { 
     console.log('App listening on port 8200');
 });
+
+//This is what I had to do to get the Amazone EC2 instance to redirect
+//from 8200 to 80. Works until we update the env file on the server or some
+//other solution
+
+//ec2 ip address: ec2-34-238-240-14.compute-1.amazonaws.com
