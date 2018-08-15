847a3daeab2caf9bae5a4617831e4971a2e088fd
Josh <joshuablankenshipnola@gmail.com>
2018-08-15 11:42:23 -0500

    reformat

--- database-mySql/index.js
+++ database-mySql/index.js
@@ -1,16 +1,16 @@
-const mysql = require('mysql');
-const mysqlConfig = require('./config.js');
-const connection = mysql.createConnection(mysqlConfig);
+// const mysql = require('mysql');
+// const mysqlConfig = require('./../config.js');
+// const connection = mysql.createConnection(mysqlConfig);
 
 
 
-connection.connect( (err)=>{
-  console.log('attempting to connect to database');
-  if (err) { 
-    console.error(err); 
-    return;  
-  }
-  console.log( "connected to database" );
-});
+// connection.connect( (err)=>{
+//   console.log('attempting to connect to database');
+//   if (err) { 
+//     console.error(err); 
+//     return;  
+//   }
+//   console.log( "connected to database" );
+// });
 
-module.exports = connection; 
+// module.exports = connection; 
--- helpers.js
+++ helpers.js
@@ -1,24 +1,23 @@
 const axios = require('axios');
 const MAPQUESTKEY = require('./config.js');
 const fetch = require('node-fetch');
-const cheerio = require('cheerio');
 const scrapeIt = require("scrape-it");
 
 // https://en.wikipedia.org/wiki/Garden_District,_New_Orleans
 
 //Cheerio is an html parser
 //get the entire http content for the first search result
-exports.getPOINarrow = (lat, long)=> {
-    axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&prop=coordinates%7Cpageimages%7Cpageterms%7Cextracts&exlimit=5&generator=geosearch&colimit=1&piprop=thumbnail&pithumbsize=144&pilimit=10&wbptterms=description&ggscoord=${lat}%7C${long}&ggsradius=100&ggslimit=10`).then(function (res) {
-        console.log(res.data.query);
-        return res.data.query;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-};
+// exports.getPOINarrow = (lat, long)=> {
+//     axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&prop=coordinates%7Cpageimages%7Cpageterms%7Cextracts&exlimit=5&generator=geosearch&colimit=1&piprop=thumbnail&pithumbsize=144&pilimit=10&wbptterms=description&ggscoord=${lat}%7C${long}&ggsradius=500&ggslimit=1`).then(function (res) {
+//         console.log(res.data.query);
+//         return res.data.query;
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+// };
 
-exports.getNeighborhood = (lat, long)=> {
+exports.getNeighborhood = (lat, long, req, res)=> {
   const endpointUrl = 'https://query.wikidata.org/sparql',
   sparqlQuery = `SELECT ?place ?location ?distance ?placeLabel WHERE {
     SERVICE wikibase:around { 
@@ -31,18 +30,20 @@ exports.getNeighborhood = (lat, long)=> {
     } ORDER BY ?distance LIMIT 10`,
   fullUrl = endpointUrl + '?query=' + encodeURIComponent(sparqlQuery),
   headers = { 'Accept': 'application/sparql-results+json' };
-
+const hood = [];
 fetch( fullUrl, { headers } ).then( body => body.json() ).then( json => {
 const { head: { vars }, results } = json;
 for ( const result of results.bindings ) {
     for ( const variable of vars ) {
-        console.log( '%s: %o', variable, result[variable] );
+      console.log( variable, result[variable] );
+        hood.push(variable, result[variable]);
     }
-    console.log( '---' );
 }
+// console.log(hood);
+res.send(hood);
 } );  
 }
-exports.getFullPage = (title)=> {
+exports.getFullPage = (title, req, res)=> {
   title = title.split(' ').join('_');
   scrapeIt(`https://en.wikipedia.org/wiki/${title}`, {
     title: 'h1',
@@ -53,12 +54,12 @@ exports.getFullPage = (title)=> {
     results = results.replace(/[\r\n]/g, "");
     results = results.split('.');
     console.log(results);
-    return results;
+    res.send(results)
 }).catch(function (error) {
   console.log(error);
 });
 };
-exports.getNeighborhoodMap = (lat, long)=> {
+exports.getNeighborhoodMap = (lat, long, req, res)=> {
   const endpointUrl = 'https://query.wikidata.org/sparql',
       sparqlQuery = `#defaultView:Map{"layer":"?instance_ofLabel"}
 SELECT ?place ?placeLabel ?image ?coordinate_location ?dist ?instance_of ?instance_ofLabel WHERE {
@@ -88,7 +89,7 @@ fetch( fullUrl, { headers } ).then( body => body.json() ).then( json => {
 //get the address at the current lat and long
 // MapQuet API key is required
 // https://www.mapquestapi.com/geocoding/v1/reverse?key=KEY&location=29.92878%2C-90.08422&outFormat=json&thumbMaps=false
-exports.getAddress = (lat, long)=> {
+exports.getAddress = (lat, long, req, res)=> {
   axios.get(`https://www.mapquestapi.com/geocoding/v1/reverse?key=${MAPQUESTKEY.MAPQUESTKEY}&location=${lat}%2C${long}&outFormat=json&thumbMaps=false`).then(function (res) {
       console.log(res.data.results[0].locations[0].street);
       return res.data.results[0].locations[0].street;
@@ -99,7 +100,7 @@ exports.getAddress = (lat, long)=> {
     // https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=1403+Washington+Ave
   }
 
-    exports.searchByAddress = (add)=> {
+    exports.searchByAddress = (add, req, res)=> {
       add = add.split(' ').join('+');
       axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${add}+New+Orleans`).then(function (res) {
         console.log(res.data.query);
@@ -109,7 +110,7 @@ exports.getAddress = (lat, long)=> {
         console.log(error);
       });
     }
-      exports.searchByTitle = (title)=> {
+      exports.searchByTitle = (title, req, res)=> {
         title = title.split(' ').join('+');
         axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${title}`).then(function (res) {
           console.log(res.data.query);
