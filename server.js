cb50de2ee30fbc0f2471aeefdaafa75ab22fee17
Josh <joshuablankenshipnola@gmail.com>
2018-08-14 20:25:01 -0500

    got full content with helper

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const bodyParser = require('body-parser');
 const helpers = require('./helpers.js');
 
 const app = express(); // (2)
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-helpers.getNeighborhood(29.92878, -90.08422);
+// helpers.getNeighborhoodMap(29.92878, -90.08422);
 // helpers.searchByAddress('1403 Washington Ave');
-
+// helpers.searchByTitle('Garden District, New Orleans');
+helpers.getFullPage('Garden District, New Orleans');
 app.listen( 8200, function() { 
     console.log('App listening on port 8200');
 });
