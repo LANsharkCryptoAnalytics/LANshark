9620e8c7a2aecf7a684b9daf3753feb4e637825e
Josh <joshuablankenshipnola@gmail.com>
2018-08-22 15:41:29 -0500

    fixed neighborhood bug

--- server.js
+++ server.js
@@ -67,10 +67,8 @@ app.get('/neighborhood', (req, res) => {
 
 app.get('/broad', (req, res) => {
     let i = req.query.i ? req.query.i : 0;
-    helpers.getNeighborhood(req.query.latitude.slice(0,9), req.query.longitude.slice(0,10)).then(body => body.json()).then((json)=>{  
-        let neighborhoods = helpers.formatNeighborhoodData(json).filter(n => {
-            return n.type === "neighborhood";
-        });
+    helpers.getNeighborhood(29.9754668,-90.0774691).then(body => body.json()).then((json)=>{  
+        let neighborhoods = helpers.formatNeighborhoodData(json);
         //filter out the neighborhood results
         if(i >0){ neighborhoods = helpers.formatNeighborhoodData(json).filter(n => {
             return n.type !== "neighborhood";
@@ -91,20 +89,22 @@ app.get('/broad', (req, res) => {
                 helpers.getFullPage(neighborhoods[i].title).then(({ data, response }) => {
                     let results = helpers.formatResults(data.paragraph);
                     // console.log(results);
-                    res.send(results);
+                    
                 
                 if(data.paragraph.length < 100){
-                    helpers.getPOINarrow( req.query.latitude.slice(0,9), req.query.longitude.slice(0,10)).then(stuff=> {
+                    helpers.getPOINarrow(29.9754668,-90.0774691).then(stuff=> {
                         // console.log(stuff.data.query);
-                        results = stuff.data.query.pages[Object.keys(stuff.data.query.pages)].extract.replace(/[\r\n]/g, "");
-                        results = results.replace(/<[^>]+>/g, ' ')
-                        results = results.replace('  ', ' ').trim();
-                        results = results.split('.');
+                       let results = helpers.formatResults(stuff.data.query.pages[Object.keys(stuff.data.query.pages)].extract.replace(/[\r\n]/g, ""));
+                        // results = results.replace(/<[^>]+>/g, ' ')
+                        // results = results.replace('  ', ' ').trim();
+                        // results = results.split('.');
                         res.send(results);
                     })
                     .catch(function (error) {
                       console.log(error);
                     });
+                }else{
+                    res.send(results);
                 }
             }).catch(function (error) {
                 console.log(error);
