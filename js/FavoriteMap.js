c7130ba763eb49b4d5d1a33d9ad821eb492f85a7
alecgilchrist <alecgilchrist@gmail.com>
2018-08-30 14:03:01 -0500

    Tabs converted to spaces. removed state, Commented out constructor

--- js/FavoriteMap.js
+++ js/FavoriteMap.js
@@ -9,27 +9,19 @@ import {
   WebView,
 } from 'react-native';
 import axios from 'axios';
-var favs2 = '';
-export default class FavoriteMap extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      favs: '',
 
-    };
-    
-  }
+let favs2 = '';
+
+export default class FavoriteMap extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
 
   componentDidMount() {
     axios.get(`http://ec2-54-152-18-28.compute-1.amazonaws.com/getUserFavorites?id=${this.props.user.id}`, {
 
     }).then((favorites) => {
       favs2 = JSON.stringify(favorites.data);
-      console.warn(favorites.data);
-      this.setState(prevState => ({
-        favs: favorites.data,
-        
-      }));
     })
       .catch((error) => { throw error; });
   }
@@ -50,8 +42,6 @@ export default class FavoriteMap extends Component {
   // };
 
   render() {
-
-    
     return (
       <View style={{ flex: 1 }}>
         <View style={{ flex: 1 }}>
@@ -62,36 +52,36 @@ export default class FavoriteMap extends Component {
               <!DOCTYPE html>
 <html>
 <head>
-	<title>Favorites Map</title>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-	<link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />
+  <title>Favorites Map</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA==" crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js" integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA==" crossorigin=""></script>
 	<style>
-		html, body {
-			height: 100%;
-			margin: 0;
-		}
-		#map {
-			width: 600px;
-			height: 400px;
-		}
-	</style>
-	<style>body { padding: 0; margin: 0; } #map { height: 100%; width: 100vw; }</style>
+    html, body {
+      height: 100%;
+      margin: 0;
+    }
+    #map {
+      width: 600px;
+      height: 400px;
+    }
+  </style>
+  <style>body { padding: 0; margin: 0; } #map { height: 100%; width: 100vw; }</style>
 </head>
 <body>
 <div id='map'></div>
 <script>
 var favs = ${favs2}
   var map = L.map('map').setView([${String(this.props.lat).slice(0, 9)}, ${String(this.props.long).slice(0, 10)}], 16);
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-			'<a href="https://creativecommons.org/licenses/by-sa/2.0/"></a>, ' +
-			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		id: 'mapbox.streets'
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/"></a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
   }).addTo(map);
   var LeafIcon = L.Icon.extend({
     options: {
@@ -113,22 +103,22 @@ for( let i = 0; i < favs.length; i++){
 
 }
     
-	L.marker([${String(this.props.lat).slice(0, 9)}, ${String(this.props.long).slice(0, 10)}], {icon: starIcon}).addTo(map)
-		.bindPopup("<b>You are here</b><br />");
-	var popup = L.popup();
-	map.on('click', onMapClick);
+  L.marker([${String(this.props.lat).slice(0, 9)}, ${String(this.props.long).slice(0, 10)}], {icon: starIcon}).addTo(map)
+    .bindPopup("<b>You are here</b><br />");
+  var popup = L.popup();
+  map.on('click', onMapClick);
 </script>
 </body>`,
               }}
-  style={{ flex: 1 }}
-  scalesPageToFit
-  onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest} // for iOS
-  onNavigationStateChange={this.onShouldStartLoadWithRequest}
-  domStorageEnabled
-          />
+              style={{ flex: 1 }}
+              scalesPageToFit
+              onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest} // for iOS
+              onNavigationStateChange={this.onShouldStartLoadWithRequest}
+              domStorageEnabled
+            />
           )
-            :(
-            <View>
+            : (
+              <View>
               <Text>Loading</Text>
             </View>
             )}
