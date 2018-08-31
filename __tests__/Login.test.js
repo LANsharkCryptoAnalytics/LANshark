0c608b75f8e7d6292f01984b52d45476ba93bb6c
SenaiAyalew <senaiayalew@gmail.com>
2018-08-30 22:15:26 -0500

    Commiting and reloading window

--- __tests__/Login.test.js
+++ __tests__/Login.test.js
@@ -10,17 +10,17 @@ test('Login snapShot', () => {
 expect(snap).toMatchSnapshot();
 });
 
-// test('expect login to be a function', () => {
-//   expect(typeof Login).toEqual('function');
-// });
+test('expect login to be a function', () => {
+  expect(typeof Login).toEqual('function');
+});
 
-// test('expect login to be defined', () => {
-//   expect(Login).toBeDefined();
-// });
+test('expect login to be defined', () => {
+  expect(Login).toBeDefined();
+});
 
-// test('expect state to be empty', () => {
-//   expect(Login._submit).toBeDefined();
-// });
+test('expect state to be empty', () => {
+  expect(Login._submit).toBeDefined();
+});
 
 // test('expect the return of submit to be an object', () => {
 //   expect.assertions(1);
